2aef656d7403ae6a1a10ade2a2639b1814ad8b52
CommanderCave <88639480+CommanderCave@users.noreply.github.com>
2021-08-16 11:21:07 +0700

    pycharm.py

---
+++ scratch.py
@@ -0,0 +1,46 @@
+name = "Cave"
+age = 14
+male = True
+women = False
+money = 3000
+score = 10
+
+print(type(name),name)
+print(type(age),age)
+print(type(male),male)
+print(type(women),women)
+print(type(money),money)
+
+
+#operator
+a=10
+b=20
+print(a+b)
+
+c=20
+d=10
+print(c-d)
+
+e=3
+f=4
+print(e*f)
+
+g=30
+h=10
+print(g/h)
+
+#loop
+for i in range(10):
+    i=i+1
+    if i>=5 and i<9:
+        print(i)
+
+
+#Ex
+number = 16
+friend = "Mu"
+friend_gender = "female"
+for j in range(number):
+    print(friend)
+    if j>=7 and j <12:
+        print(j)
