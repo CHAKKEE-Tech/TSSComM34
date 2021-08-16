e640ec49ee5700c46738d949f2a413bb5c0db8f7
FezkFezk <88639472+FezkFezk@users.noreply.github.com>
2021-08-16 11:13:22 +0700

    [add]

---
+++ 19.flukfluk.py
@@ -0,0 +1,35 @@
+name = "Ratchakrit Yodkaew"
+age = 15
+men = True
+women = False
+money = 100
+print(type(name), name)
+print(type(age), age)
+print(type(men), men)
+print(type(women), women)
+print(type(money), money)
+#operator
+a = 10
+b = 20
+print(a+b)
+print(b-a)
+print(a/b)
+print(a*b)
+
+
+
+#loop
+for i in range(10):
+    i = i+1
+    if i>=5 and i<9:
+     print(i)
+
+
+
+
+#ex
+number = 16
+friend = "cave"
+friend_gender = "men"
+for C in range(number):
+ print(friend)
