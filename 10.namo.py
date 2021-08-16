308c3a12de0080084bcf8d4823490a9aed80b201
Namo7327 <88639420+Namo7327@users.noreply.github.com>
2021-08-16 11:30:32 +0700

    [add]

---
+++ 10.namo.py
@@ -0,0 +1,31 @@
+name = "Namo "
+age = 15
+men = True
+women = False
+money = 4000.2
+
+
+print(type(name),name)
+print(type(age),age)
+print(type(men),men)
+print(type(money),money)
+
+#operator
+a = 10
+b = 20
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+
+#loop
+for i in range(10):
+    i = i+1
+    if 1>5:
+      print(i)
+number = 16
+friend = "Ham"
+friend_gender = "male"
+for e in range(number):
+   if e>=7 and e< 13:
+     print(e)
