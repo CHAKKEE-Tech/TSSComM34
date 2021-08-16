777c7dae27196880617fce3fa10bcf87fe991df8
ChakSGT <38743018+ChakSGT@users.noreply.github.com>
2021-08-16 11:08:08 +0700

    [ADD]

---
+++ 0.chak.py
@@ -0,0 +1,32 @@
+#Data type
+name = "Wiroot Klakhaeng"
+age = 27
+men = True
+Women = False
+money = 20.37
+score = [9,10,7,0]
+
+
+#operator
+a=10
+b=20
+
+#loop condition
+for i in range(10):
+    i = i+1
+    if i>=5 and i<9:
+        print(i)
+
+#Ex
+number = 16
+friend = ""
+friend_gender = ""
+for j in range(number):
+    if j>=7 and j < 13
+
+
+
+
+
+
+
