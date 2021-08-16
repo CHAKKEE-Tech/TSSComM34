2dffe62a1206668433156ae862aaea180e8cc2e0
Greeny50 <88639442+Greeny50@users.noreply.github.com>
2021-08-16 11:21:24 +0700

    [ADD]

---
+++ 8.Green.py
@@ -0,0 +1,33 @@
+name = "Thitirat Yuenyan"
+age = 14
+men = False
+women = True
+money = 50.99
+
+print(type(name), name)
+print(type(age), age)
+print(type(women), women)
+print(type(money), money)
+
+#operater
+a = 10
+b = 20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#loop condition
+for i in range(10):
+    i = i+i
+    if i>=5 and i <9:
+        print(i)
+
+#Ex
+number = 21
+friend = "Mu"
+friend_gender = "women"
+for m in range(number):
+    m = m+1
+    if m>=7 and m <12:
+        print(m)
