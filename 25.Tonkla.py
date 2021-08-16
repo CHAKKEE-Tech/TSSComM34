1a462e49197945bfbf6091ddc69aea8f78551050
Hatsapong Fagsukjit <hatsaphong1310@gmail.com>
2021-08-16 11:08:08 +0700

    [ADD]

---
+++ 25.Tonkla.py
@@ -0,0 +1,41 @@
+name = "Hatsapong Fagsukjit"
+age = 15
+men = True
+women = False
+money = 20.37
+
+print(type(name), name)
+print(type(age), age)
+print(type(men), men)
+print(type(women), women)
+print(type(money), money)
+
+#operator
+a=10
+b=20
+print(a+b)
+c=50
+d=20
+print(c-d)
+a=40
+b=20
+print(a/b)
+c=20
+d=10
+print(c*d)
+
+#loop
+for i in range(10):
+    i = i+1
+    if i>=5 and i<9:
+        print(i)
+
+
+#Ex
+number = 16
+friend = "rainy"
+friend_gender = "Female"
+for g in range(number):
+    if g>=7 and g < 13:
+        print(g)
+
