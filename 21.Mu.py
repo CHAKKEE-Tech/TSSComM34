6cb1e9639848a0e237935e0099b6f3ffb16e691a
mucumber <waewprat@hotmail.com>
2021-08-16 11:31:37 +0700

    [ADD]

---
+++ 21.Mu.py
@@ -0,0 +1,36 @@
+name = "Waewprat Himananto"
+age = 14
+men = False
+women = True
+money = 70
+
+print(type(name), name)
+print(type(age), age)
+print(type(men), men)
+print(type(women), women)
+print(type(money), money)
+
+#operator
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#loop condition
+for i in range(10):
+    i = 1+1
+    if i>=5 and 1<9:
+        print(i)
+#Ex
+number = 22
+friend = "Tho"
+friend_gender = "women"
+for k in range(number):
+    k = k+1
+    if k>=7 and k <12:
+        print(k)
+
+
+
